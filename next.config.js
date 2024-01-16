a6941ee19200b195700613dc2a9c451fa62dc592
Hugo Migner <hugo.migner@gmail.com>
2024-01-16 10:06:19 -0500

    Change to Supabase

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    transpilePackages: ['react-daisyui'],
-    reactStrictMode: true,
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+module.exports = nextConfig;

89c0148897b53a7b86961599ef53ec24aedc7054
Samuel Montambault <mont.samuel@outlook.com>
2024-01-31 15:38:41 -0500

    docker cd

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone'
 }
 
 module.exports = withNextIntl(nextConfig)

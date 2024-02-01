433959dfbdee73b7cfe57f376c11362512897349
Antoine Martineau <martineau.antoin3@gmail.com>
2024-02-01 10:44:41 -0500

    Constants update

--- constants.ts
+++ constants.ts
@@ -1,11 +1,39 @@
 export default {
-    newsStatuses: {
-        all: 1,
-        published: 2,
-        draft: 3,
-        onHold: 4,
-        refused: 5,
-        approved: 6,
-        deleted: 7
-    }
+    newsStatuses: [
+        {
+            id: 1,
+            color: "orange",
+            label: "all",
+        },
+        {
+            id: 2,
+            color: "blue",
+            label: "published",
+        },
+        {
+            id: 3,
+            color: "green",
+            label: "draft",
+        },
+        {
+            id: 4,
+            color: "pink",
+            label: "on-hold",
+        },
+        {
+            id: 5,
+            color: "purple",
+            label: "refused",
+        },
+        {
+            id: 6,
+            color: "error",
+            label: "approved",
+        },
+        {
+            id: 7,
+            color: "error",
+            label: "deleted",
+        }
+    ]
 }

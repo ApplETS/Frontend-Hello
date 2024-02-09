168205920bd3768492fa9cdc76965daa9d2b21c3
Hugo Migner <hugo.migner@gmail.com>
2024-02-09 11:44:05 -0500

    Connect profile page to backend

--- app/[locale]/dashboard/settings/profile/page.tsx
+++ app/[locale]/dashboard/settings/profile/page.tsx
@@ -1,8 +1,10 @@
-import Dropzone from "@/components/Dropzone";
-import Toast from "@/components/Toast";
-import { AlertType } from "@/components/Alert";
-import { unstable_setRequestLocale } from "next-intl/server";
-import { useTranslations } from "next-intl";
+import Dropzone from '@/components/Dropzone';
+import Toast from '@/components/Toast';
+import { AlertType } from '@/components/Alert';
+import { unstable_setRequestLocale } from 'next-intl/server';
+import { useTranslations } from 'next-intl';
+import Dropdown from '@/components/SignUpActivity';
+import { updateProfile } from '@/utils/supabase/auth';
 
 type Props = {
 	params: { locale: string };
@@ -10,64 +12,42 @@ type Props = {
 
 export default function Page({ params }: Props) {
 	unstable_setRequestLocale(params.locale);
-	const t = useTranslations("Settings.profile-section");
+	const t = useTranslations('Settings.profile-section');
 	return (
-		<form className="flex flex-col basis-3/4">
-			<input type="hidden" name="locale" value={params.locale} />
-			<div className="flex-grow">
-				<label className="text-xl font-bold">{t("title")}</label>
-				<div className="grid grid-cols-4 gap-6 justify-left items-center pt-10">
-					<div className="avatar placeholder">
-						<div className="bg-neutral text-neutral-content rounded-full w-36">
-							<span className="text-3xl">D</span>
+		<form className='flex flex-col basis-3/4' action={updateProfile}>
+			<input type='hidden' name='locale' value={params.locale} />
+			<div className='flex-grow'>
+				<label className='text-xl font-bold'>{t('title')}</label>
+				<div className='grid grid-cols-4 gap-6 justify-left items-center pt-10'>
+					<div className='avatar placeholder'>
+						<div className='bg-neutral text-neutral-content rounded-full w-36'>
+							<span className='text-3xl'>D</span>
 						</div>
 					</div>
 					<Dropzone />
-					<div className="col-span-2" />
-					<label>{t("companyName")}</label>
-					<input
-						type="text"
-						className="input input-ghost input-bordered border-current"
-						name="organisation"
-					/>
+					<div className='col-span-2' />
+					<label>{t('companyName')}</label>
+					<input type='text' className='input input-ghost input-bordered border-current' name='organization' />
 
-					<label className="justify-self-center">{t("description")}</label>
-					<textarea
-						className="textarea textarea-ghost border-current row-span-2 h-full"
-						name="description"
-					/>
+					<label className='justify-self-center'>{t('description')}</label>
+					<textarea className='textarea textarea-ghost border-current row-span-2 h-full' name='description' />
 
-					<label>{t("email")}</label>
-					<input
-						type="text"
-						className="input input-ghost input-bordered border-current"
-						name="email"
-						required
-					/>
+					<label>{t('email')}</label>
+					<input type='text' className='input input-ghost input-bordered border-current' name='email' required />
 
 					<div />
 
-					<label className="">{t("activity")}</label>
-					<input
-						type="text"
-						className="input input-ghost input-bordered border-current"
-						name="theme"
-					/>
+					<label className=''>{t('activity')}</label>
+					<Dropdown items={[t('scientificClub'), t('ets'), t('sve'), t('aeets')]} inputName='activity' />
 
-					<label className="justify-self-center">{t("website")}</label>
-					<input
-						type="text"
-						className="input input-ghost input-bordered border-current"
-						name="activity"
-					/>
+					<label className='justify-self-center'>{t('website')}</label>
+					<input type='text' className='input input-ghost input-bordered border-current' name='website' />
 
-					<div className="col-span-2" />
+					<div className='col-span-2' />
 				</div>
 			</div>
-			<footer className="sticky flex justify-end mt-auto">
-				<button className="btn btn-primary rounded-md text-base w-1/5 mt-auto">
-					{t("save")}
-				</button>
+			<footer className='sticky flex justify-end mt-auto'>
+				<button className='btn btn-primary rounded-md text-base w-1/5 mt-auto'>{t('save')}</button>
 			</footer>
 		</form>
 	);
--- utils/supabase/auth.ts
+++ utils/supabase/auth.ts
@@ -1,14 +1,14 @@
-import { headers, cookies } from "next/headers";
-import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import { api } from "@/config";
+import { headers, cookies } from 'next/headers';
+import { createClient } from '@/utils/supabase/server';
+import { redirect } from 'next/navigation';
+import { api } from '@/config';
 
 export const signIn = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const email = formData.get("email") as string;
-	const password = formData.get("password") as string;
-	const locale = formData.get("locale") as string;
+	const email = formData.get('email') as string;
+	const password = formData.get('password') as string;
+	const locale = formData.get('locale') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 
@@ -25,12 +25,12 @@ export const signIn = async (formData: FormData) => {
 };
 
 export const signUp = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const origin = headers().get("origin");
-	const email = formData.get("email") as string;
-	const password = formData.get("password") as string;
-	const locale = formData.get("locale") as string;
+	const origin = headers().get('origin');
+	const email = formData.get('email') as string;
+	const password = formData.get('password') as string;
+	const locale = formData.get('locale') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 
@@ -48,18 +48,16 @@ export const signUp = async (formData: FormData) => {
 
 	const userObject = {
 		id: data?.user?.id,
-		name: formData.get("name") as string,
 		email: email,
-		password: password,
-		organisation: formData.get("name") as string,
-		activityArea: formData.get("activity") as string,
+		organisation: formData.get('name') as string,
+		activityArea: formData.get('activity') as string,
 	};
 
 	const response = await fetch(`${api}/user/organizer`, {
-		method: "POST",
+		method: 'POST',
 		headers: {
-			"Content-Type": "application/json",
-			Authorization: "Bearer " + data?.session?.access_token,
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + data?.session?.access_token,
 		},
 		body: JSON.stringify(userObject),
 	});
@@ -71,46 +69,40 @@ export const signUp = async (formData: FormData) => {
 };
 
 export const forgotPassword = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const email = formData.get("email") as string;
-	const locale = formData.get("locale") as string;
+	const email = formData.get('email') as string;
+	const locale = formData.get('locale') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 
 	const { error } = await supabase.auth.resetPasswordForEmail(email);
 
 	if (error) {
-		return redirect(
-			`/${locale}/forgotpassword?code=${error.status}&type=error`
-		);
+		return redirect(`/${locale}/forgotpassword?code=${error.status}&type=error`);
 	}
 
-	return redirect(
-		`/${locale}/updatepassword?message=Veuillez consulter votre email&email=${email}&type=warning`
-	);
+	return redirect(`/${locale}/updatepassword?message=Veuillez consulter votre email&email=${email}&type=warning`);
 };
 
 export const updatePassword = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const email = formData.get("email") as string;
-	const password = formData.get("password") as string;
-	const token = formData.get("token") as string;
-	const locale = formData.get("locale") as string;
+	const email = formData.get('email') as string;
+	const password = formData.get('password') as string;
+	const token = formData.get('token') as string;
+	const locale = formData.get('locale') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 
 	const resOtp = await supabase.auth.verifyOtp({
 		email: email,
 		token: token,
-		type: "recovery",
+		type: 'recovery',
 	});
 
 	if (resOtp.error) {
-		return redirect(
-			`/${locale}/updatepassword?code=${resOtp.error.status}&type=error`
-		);
+		return redirect(`/${locale}/updatepassword?code=${resOtp.error.status}&type=error`);
 	}
 
 	const { error } = await supabase.auth.updateUser({
@@ -118,28 +110,24 @@ export const updatePassword = async (formData: FormData) => {
 	});
 
 	if (error) {
-		return redirect(
-			`/${locale}/updatepassword?code=${error.status}&type=error`
-		);
+		return redirect(`/${locale}/updatepassword?code=${error.status}&type=error`);
 	}
 
 	return redirect(`/${locale}/dashboard`);
 };
 
 export const updatePasswordSettings = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const email = formData.get("email") as string;
-	const password = formData.get("password") as string;
-	const confirmPassword = formData.get("confirmPassword") as string;
-	const locale = formData.get("locale") as string;
+	const email = formData.get('email') as string;
+	const password = formData.get('password') as string;
+	const confirmPassword = formData.get('confirmPassword') as string;
+	const locale = formData.get('locale') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 
 	if (password !== confirmPassword) {
-		return redirect(
-			`/${locale}/dashboard/settings/password?code=400&type=error`
-		);
+		return redirect(`/${locale}/dashboard/settings/password?code=400&type=error`);
 	}
 
 	const { error } = await supabase.auth.updateUser({
@@ -148,23 +136,61 @@ export const updatePasswordSettings = async (formData: FormData) => {
 
 	if (error) {
 		console.log(error);
-		return redirect(
-			`/${locale}/dashboard/settings/password?code=${error.status}&type=error`
-		);
+		return redirect(`/${locale}/dashboard/settings/password?code=${error.status}&type=error`);
 	}
 
-	return redirect(
-		`/${locale}/dashboard/settings/password?code=200&type=success`
-	);
+	return redirect(`/${locale}/dashboard/settings/password?code=200&type=success`);
 };
 
 export const signOut = async (formData: FormData) => {
-	"use server";
+	'use server';
 
-	const redirectLink = formData.get("redirectLink") as string;
+	const redirectLink = formData.get('redirectLink') as string;
 	const cookieStore = cookies();
 	const supabase = createClient(cookieStore);
 	await supabase.auth.signOut();
 
 	return redirect(redirectLink);
+};
+
+export const updateProfile = async (formData: FormData) => {
+	'use server';
+
+	const locale = formData.get('locale') as string;
+	const cookieStore = cookies();
+	const supabase = createClient(cookieStore);
+
+	const { data, error } = await supabase.auth.getUser();
+
+	if (error) {
+		return redirect(`/${locale}/dashboard/settings/profile?code=${error.status}&type=error`);
+	}
+
+	const { data: sessionData, error: sessionError } = await supabase.auth.getSession();
+
+	if (sessionError) {
+		return redirect(`/${locale}/dashboard/settings/profile?code=${sessionError.status}&type=error`);
+	}
+
+	const userObject = {
+		id: data.user?.id,
+		email: formData.get('email') as string,
+		organisation: formData.get('organization') as string,
+		activityArea: formData.get('activity') as string,
+	};
+
+	const response = await fetch(`${api}/user`, {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: 'Bearer ' + sessionData.session?.access_token,
+		},
+		body: JSON.stringify(userObject),
+	});
+
+	if (!response.ok) {
+		return redirect(`/${locale}/dashboard/settings/profile?code=${response.status}&type=error`);
+	}
+
+	return redirect(`/${locale}/dashboard/settings/profile?code=200&type=success`);
 };

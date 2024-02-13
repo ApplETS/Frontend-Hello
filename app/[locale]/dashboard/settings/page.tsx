91b7b94864af6c5f5bfa0ac414c959edf21185d5
Hugo Migner <hugo.migner@gmail.com>
2024-02-13 16:51:16 -0500

    Fix build pour devrai

--- app/[locale]/dashboard/settings/page.tsx
+++ app/[locale]/dashboard/settings/page.tsx
@@ -1,23 +1,5 @@
-import { AlertType } from '@/components/Alert';
-import Dropzone from '@/components/Dropzone';
-import PasswordInput from '@/components/PasswordInput';
-import Toast from '@/components/Toast';
-import { updatePasswordSettings } from '@/utils/supabase/auth';
-import { useTranslations } from 'next-intl';
-import { unstable_setRequestLocale } from 'next-intl/server';
-import React, { useState } from 'react';
-import Profil from './profile/Page.1';
-import MotDePasse from './password/page';
-import Apparence from './display/page';
-import ReseauxSociaux from './socials/page';
 import { redirect } from 'next/navigation';
 
-export default function Page({
-	searchParams,
-	params,
-}: {
-	searchParams: { message: string; type: string; code: string };
-	params: { locale: string };
-}) {
+export default function Page({ params }: { params: { locale: string } }) {
 	return redirect(`/${params.locale}/dashboard/settings/profile`);
 }

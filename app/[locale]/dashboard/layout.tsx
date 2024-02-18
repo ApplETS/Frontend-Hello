629d2d57b526a5c8737c15ecd084cbbec6b761ca
JonathanDuvalV <duvalvenne.jonathan@gmail.com>
2024-02-18 15:13:08 -0500

    Hide Approbation in navbar for non-moderator

--- app/[locale]/dashboard/layout.tsx
+++ app/[locale]/dashboard/layout.tsx
@@ -1,32 +1,37 @@
 import React, { ReactElement } from "react";
 import DashboardLayout from "./components/dashboardLayout";
-import { useTranslations } from "next-intl";
-import { unstable_setRequestLocale } from "next-intl/server";
+import { getTranslations, unstable_setRequestLocale } from "next-intl/server";
+import { getAuthenticatedUser } from "@/lib/get-authenticated-user";
 
 type Props = {
-  children: ReactElement;
-  params: { locale: string };
+	children: ReactElement;
+	params: { locale: string };
 };
 
-export default function Layout({ children, params: { locale } }: Props) {
-  unstable_setRequestLocale(locale);
+// This component should always stay server-side or else there will be security issues with the user type checking
+export default async function Layout({ children, params: { locale } }: Props) {
+	unstable_setRequestLocale(locale);
 
-  const t = useTranslations("Dashboard");
+	const t = await getTranslations("Dashboard");
+	const user = await getAuthenticatedUser();
 
-  const pages = {
-    news: {
-      title: t("news"),
-      link: `/${locale}/news`,
-    },
-    publications: {
-      title: t("publications"),
-      link: `/${locale}/dashboard/publications`,
-    },
-    approbations: {
-      title: t("approbations"),
-      link: `/${locale}/dashboard/approbations`,
-    },
-  };
+	var pages = {
+		news: {
+			title: t("news"),
+			link: `/${locale}/news`,
+			isVisible: true,
+		},
+		publications: {
+			title: t("publications"),
+			link: `/${locale}/dashboard/publications`,
+			isVisible: true,
+		},
+		approbations: {
+			title: t("approbations"),
+			link: `/${locale}/dashboard/approbations`,
+			isVisible: user.type == "Moderator",
+		},
+	};
 
-  return <DashboardLayout pages={pages}>{children}</DashboardLayout>;
+	return <DashboardLayout pages={pages}>{children}</DashboardLayout>;
 }
---
+++ lib/get-authenticated-user.tsx
@@ -0,0 +1,20 @@
+import { fetchWithSession, Method } from "@/lib/fetch-with-refresh";
+import { ApiResponse } from "@/models/api-response";
+import { User } from "@/models/user";
+
+export async function getAuthenticatedUser(): Promise<User> {
+	const response = await fetchWithSession(`user`, Method.GET);
+
+	if (!response.ok) {
+		throw new Error("Failed the authenticated user information");
+	}
+
+	const responseData: ApiResponse<User> = await response.json();
+
+	// Check for errors in response
+	if (responseData.error) {
+		throw new Error("Error in response data");
+	}
+
+	return responseData.data;
+}

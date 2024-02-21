0b8d805b4010f8eee9683dec7de13cb0648584e9
Antoine Martineau <47289926+Udito3@users.noreply.github.com>
2024-02-21 10:53:32 -0500

    Black background

--- components/modals/Preview.tsx
+++ components/modals/Preview.tsx
@@ -31,8 +31,8 @@ export default function PublicationDetails({ props }: PublicationDetailsProps) {
 	const { isLight } = useTheme();
 
 	return (
-		<div className="inset-0 bg-black bg-opacity-10 z-50">
-			<dialog id="publication_modal" className="modal overflow-y-auto" open={true}>
+		<div className="inset-0 z-50">
+			<dialog id="publication_modal" className="modal bg-black bg-opacity-20 overflow-y-auto" open={true}>
 				<div className="mockup-phone border-primary w-80 overflow-y-auto p-0 m-0">
 					<div className="camera"></div>
 					<div className="display">

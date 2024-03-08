62f1b3db2b2abc5881e36a7b70c8b9e60babf76c
Camille Brulotte <camille.brulotte@icloud.com>
2024-03-08 08:05:44 -0500

    Handle clicking outside

--- components/DropdownMenu.tsx
+++ components/DropdownMenu.tsx
@@ -5,7 +5,7 @@ import { faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
 import { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 interface Props {
-	items: { text: string; icon: IconProp, color: string; }[];
+	items: { text: string; icon: IconProp; color: string }[];
 }
 
 export default function DropdownMenu({ items }: Props) {
@@ -31,24 +31,24 @@ export default function DropdownMenu({ items }: Props) {
 	}, []);
 
 	return (
-		<div className='flex items-center space-x-4' ref={dropdownRef}>
-      <div className='dropdown relative'>
-        <button className='flex items-center' onClick={toggleDropdown}>
-          <FontAwesomeIcon icon={faEllipsisVertical} className='w-5 ml-2' />
-        </button>
-        {isDropdownOpen && (
-          <ul className={"p-2 shadow menu dropdown-content bg-base-100 rounded-box absolute z-10 right-0"}>
-            {items.map((item, index) => (
-              <li className={`flex ${item.color} w-40`} key={index} onClick={handleItemClick}>
-                <a className="flex space-x-2">
-                  <FontAwesomeIcon icon={item.icon} className='w-5' />
-                  {item.text}
-                </a>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    </div>
+		<div className="flex items-center space-x-4" ref={dropdownRef}>
+			<div className="dropdown relative">
+				<button className="flex items-center" onClick={toggleDropdown}>
+					<FontAwesomeIcon icon={faEllipsisVertical} className="w-5 ml-2" />
+				</button>
+				{isDropdownOpen && (
+					<ul className={'p-2 shadow menu dropdown-content bg-base-100 rounded-box absolute z-10 right-0'}>
+						{items.map((item, index) => (
+							<li className={`flex ${item.color} w-40`} key={index} onClick={handleItemClick}>
+								<a className="flex space-x-2">
+									<FontAwesomeIcon icon={item.icon} className="w-5" />
+									{item.text}
+								</a>
+							</li>
+						))}
+					</ul>
+				)}
+			</div>
+		</div>
 	);
 }

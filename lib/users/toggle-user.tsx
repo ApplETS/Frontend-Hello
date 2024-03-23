a953d61c0b8d588975fe56749be951a8f1fa3578
Jonathan Duval-Venne <duvalvenne.jonathan@gmail.com>
2024-03-22 20:28:51 -0400

    Unused imports

--- lib/users/toggle-user.tsx
+++ lib/users/toggle-user.tsx
@@ -1,6 +1,4 @@
 import { fetchWithSession, Method } from '@/lib/fetch-with-refresh';
-import Constants from '@/utils/constants';
-import { revalidateTag } from 'next/cache';
 
 export async function toggleUser(id: string) {
 	const response = await fetchWithSession(`moderator/organizer/${id}/toggle`, Method.PATCH, null);

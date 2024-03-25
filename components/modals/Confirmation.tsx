40aaddbbd83631a02fb1dfe7d92f0fb8d88ee56a
Antoine Martineau <47289926+Udito3@users.noreply.github.com>
2024-03-25 12:35:54 -0400

    Update Confirmation.tsx

--- components/modals/Confirmation.tsx
+++ components/modals/Confirmation.tsx
@@ -53,59 +53,50 @@ export default function Confirmation({
 	};
 
 	return (
-		<>
-			<div className="fixed inset-0 bg-black bg-opacity-30 z-50 flex justify-center items-center">
-				<dialog
-					id="confirmation_modal"
-					className={`modal bg-base-200 overflow-y-auto ${
-						inputTitle ? 'p-5 w-[40rem]' : 'm-5 p-5 w-[40rem]'
-					} rounded-2xl transform -translate-x-1/2 -translate-y-1/2 ${inputTitle ? 'h-72' : 'h-40 '}`}
-					open={true}
-					style={{ top: '50%', left: '50%' }}
-				>
-					{isPending ? (
-						<div className="flex justify-center items-center w-full h-full">
-							<div className="loading loading-spinner loading-lg"></div>
+		<Modal>
+			<div id="confirmation_modal" className={`bg-base-200 overflow-y-auto p-7 max-w-[40rem] rounded-2xl max-h-full`}>
+				{isPending ? (
+					<div className="flex justify-center items-center w-full h-full">
+						<div className="loading loading-spinner loading-lg"></div>
+					</div>
+				) : (
+					<div className="flex flex-col h-full justify-between">
+						<div className="w-full">
+							<p className="text-xl text-center mb-2">{title}</p>
+							{inputTitle && (
+								<label className="w-full" htmlFor="input">
+									<div className="flex flex-col">
+										<span className="label-text text-base mt-2">{inputTitle}</span>
+										<textarea
+											name="input"
+											className="textarea textarea-ghost border-current row-span-2 h-full self-start col-span-2 w-full mt-2"
+											required
+											value={inputValue}
+											onChange={(e) => setInputValue && setInputValue(e.target.value)}
+										/>
+									</div>
+								</label>
+							)}
 						</div>
-					) : (
-						<>
-							<div className="w-full px-5">
-								<p className="text-xl text-center">{title}</p>
-								{inputTitle && (
-									<label className="w-full" htmlFor="input">
-										<div className="flex flex-col">
-											<span className="label-text text-base mt-2">{inputTitle}</span>
-											<textarea
-												name="input"
-												className="textarea textarea-ghost border-current row-span-2 h-full self-start col-span-2 w-full mt-2"
-												required
-												value={inputValue}
-												onChange={(e) => setInputValue && setInputValue(e.target.value)}
-											/>
-										</div>
-									</label>
-								)}
-							</div>
 
-							<div className={`grid grid-cols-2 gap-6 mt-2 ${inputTitle ? 'mt-5' : ''}`}>
-								<button
-									className={`btn text-black ${isLight ? 'bg-base-300 hover:bg-secondary' : 'btn-secondary'}`}
-									onClick={handleClose}
-									type="button"
-								>
-									{firstButtonTitle}
-								</button>
-								<button
-									className={`btn ${secondButtonColor} ${secondButtonHoverColor} text-black`}
-									type="button"
-									onClick={submit}
-								>
-									{secondButtonTitle}
-								</button>
-							</div>
-						</>
-					)}
-				</dialog>
+						<div className={`grid grid-cols-2 gap-6 mt-2 px-24 ${inputTitle ? 'mt-5' : ''}`}>
+							<button
+								className={`btn text-black ${isLight ? 'bg-base-300 hover:bg-secondary' : 'btn-secondary'}`}
+								onClick={handleClose}
+								type="button"
+							>
+								{firstButtonTitle}
+							</button>
+							<button
+								className={`btn ${secondButtonColor} ${secondButtonHoverColor} text-black`}
+								type="button"
+								onClick={submit}
+							>
+								{secondButtonTitle}
+							</button>
+						</div>
+					</div>
+				)}
 			</div>
 		</Modal>
 	);

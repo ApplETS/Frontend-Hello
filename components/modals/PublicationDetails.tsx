2c2da0754f84d253b6a1738b0a72692756a4b560
Hugo Migner <hugo.migner@gmail.com>
2024-03-26 18:06:36 -0400

    Fix image send

--- components/modals/PublicationDetails.tsx
+++ components/modals/PublicationDetails.tsx
@@ -192,9 +192,26 @@ export default function PublicationDetails({ locale, publication, modalMode, tag
 		setImageModalOpen(false);
 	};
 
-	const handleImageModalConfirm = (imageSrc: string) => {
-		setImageSrc(imageSrc);
-		setImageModalOpen(false);
+	const handleImageModalConfirm = async (imageSrc: string) => {
+		try {
+			// Fetch the image data
+			const response = await fetch(imageSrc);
+
+			if (!response.ok) {
+				throw new Error('Failed to fetch image data');
+			}
+
+			// Convert the response data to a blob
+			const blob = await response.blob();
+
+			// Set the blob in the state or perform any other action
+			setImageSrc(imageSrc);
+			setImageBinary(blob);
+			setImageModalOpen(false);
+		} catch (error) {
+			console.error('Error fetching image data:', error);
+			// Handle the error accordingly
+		}
 	};
 
 	const handleContentChange = (newContent: string) => {

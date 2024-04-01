70ca4892e9a15951b96d39cae6e030c7efd062a7
Hugo Migner <hugo.migner@gmail.com>
2024-04-01 10:56:45 -0400

    console log encore

--- lib/update-profile.tsx
+++ lib/update-profile.tsx
@@ -2,7 +2,6 @@ import { fetchWithSession, Method } from '@/lib/fetch-with-refresh';
 import { User } from '@/models/user';
 
 export async function updateUserProfile(userObject: User) {
-	console.log(userObject);
 	const response = await fetchWithSession(`me`, Method.PATCH, userObject);
 
 	if (!response.ok) {

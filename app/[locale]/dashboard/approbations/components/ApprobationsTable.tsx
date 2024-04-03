4688bea9bce277f45378eaab6d8d3d0e40d5ed0d
Camille Brulotte <camille.brulotte@icloud.com>
2024-04-03 19:40:52 -0400

    oops

--- app/[locale]/dashboard/approbations/components/ApprobationsTable.tsx
+++ app/[locale]/dashboard/approbations/components/ApprobationsTable.tsx
@@ -149,48 +149,6 @@ export default function ApprobationsTable({ locale, tags, id }: Props) {
 				/>
 			) : (
 				<div className="text-center py-4">{t('no-publications')}</div>
-				<table className="table w-full rounded-lg">
-					<thead className="bg-base-300 rounded-t-lg h-17">
-						<tr className="text-base-content text-base font-bold">
-							<th className="rounded-tl-lg">{t('table.author')}</th>
-							<th>{t('table.title')}</th>
-							<th>{t('table.release-date')}</th>
-							<th>{t('table.status')}</th>
-							<th className="w-[10%] rounded-tr-lg"></th>
-						</tr>
-					</thead>
-					<tbody>
-						{filteredEvents.map((event, index) => (
-							<tr key={index} className="border-b-2 border-base-300">
-								<td className="text-base flex items-center space-x-2">
-									<div className="avatar mr-3">
-										<Avatar userProfile={event.organizer} size="w-10 h-10" textSize="text-xl" color="bg-base-300" />
-									</div>
-									<div>
-										<div>{event.organizer?.organization}</div>
-										<div className="text-secondary">{event.organizer?.activityArea}</div>
-									</div>
-								</td>
-								<td>{event.title}</td>
-								<td>{formatDate(new Date(event.publicationDate), locale)}</td>
-								<td className="text-base">
-									<div
-										className={`py-4 px-4 badge ${
-											Constants.newsStatuses[event.state].color || 'badge-neutral'
-										} text-black`}
-									>
-										{t(`filters.${Constants.newsStatuses[event.state].label}`)}
-									</div>
-								</td>
-								<td className="text-base">
-									<button className="btn btn-accent w-full font-normal" onClick={() => openModal(event)}>
-										{t('table.open')}
-									</button>
-								</td>
-							</tr>
-						))}
-					</tbody>
-				</table>
 			)}
 			{isLoading && <LoadingSpinner localModal={true} />}
 		</div>

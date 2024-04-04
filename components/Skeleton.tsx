16663311f07d0dc965f9576537d0b15bb5b8ec57
Camille Brulotte <camille.brulotte@icloud.com>
2024-04-04 09:42:19 -0400

    Use daisyui skeleton

---
+++ components/Skeleton.tsx
@@ -0,0 +1,16 @@
+export default function Skeleton() {
+    return (
+      <div className="card justify-center rounded-lg bg-base-200 flex flex-col gap-4 w-full p-4">
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-44 mt-2"></div>
+        <div className="skeleton h-32 w-full mt-2"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+      </div>      
+    );
+  }
+  

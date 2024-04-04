c5cad8c908c0787440e6589f65c73cb3b326cbf6
Camille Brulotte <camille.brulotte@icloud.com>
2024-04-04 09:49:34 -0400

    Add some lines

--- components/Skeleton.tsx
+++ components/Skeleton.tsx
@@ -7,6 +7,11 @@ export default function Skeleton() {
         <div className="skeleton h-32 w-full mt-2"></div>
         <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-44"></div>
+        <div className="skeleton h-4 w-full"></div>
+        <div className="skeleton h-4 w-28"></div>
+        <div className="skeleton h-4 w-full"></div>
         <div className="skeleton h-4 w-28"></div>
       </div>      
     );
